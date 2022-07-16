9308f6c8e064c573dc0b5f25128c2d598bc9d3df
Del Reid <45520351+itsjustdel@users.noreply.github.com>
2022-07-16 23:16:59 +0100

    final

--- Trim/trim_wheel/trim_wheel.ino
+++ Trim/trim_wheel/trim_wheel.ino
@@ -1,59 +1,124 @@
 #include <Joystick.h>
+#define CLK 4
+#define DT 7
+//#define SW 7
 
-#define outputA 4
-#define outputB 7
+int sensitivity = 2;
+int xPos;
+int yPos;
+int zPos;
+
+int switchPos = 0;
+int axisRange = 512; // one way
 
 int counter = 0;
-int aState;
-int aLastState;
+int currentStateCLK;
+int lastStateCLK;
+
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
-                   1, 0,                  // Button Count, Hat Switch Count
+                   0, 0,                  // Button Count, Hat Switch Count
                    true, true, true,     // X and Y, but no Z Axis
                    false, false, false,   // No Rx, Ry, or Rz
                    false, false,          // No rudder or throttle
                    false, false, false);  // No accelerator, brake, or steering
 
 void setup() {
-  // Initialize Button Pins
+  // rotary
   pinMode(4, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
+  // switch
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
 
   // Initialize Joystick Library
   Joystick.begin();
 
-  Joystick.setXAxisRange(-512, 512);//can go 1024
-  Joystick.setYAxisRange(-128, 128);
-  Joystick.setZAxisRange(-128, 128);
+  Joystick.setXAxisRange(-axisRange, axisRange);
+  Joystick.setYAxisRange(-axisRange, axisRange);
+  Joystick.setZAxisRange(-axisRange, axisRange);
 
-  Joystick.setButton(0, 1);
+
+  // Read the initial state of CLK
+  lastStateCLK = digitalRead(CLK);
 }
 
-// Last state of the buttons
-int lastButtonState[5] = {0, 0, 0, 0, 0};
-
 void loop() {
+  rotary();
+  switch3Way();
+}
 
-  aState = digitalRead(outputA); // Reads the "current" state of the outputA
-  // If the previous and the current state of the outputA are different, that means a Pulse has occured
-  if (aState != aLastState) {
-    // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-    if (digitalRead(outputB) != aState) {
-      counter += 2;
-      if (counter > 512) {
-        counter = 512;        
-      }      
-      Joystick.setXAxis(-counter);
-    } else {
-      counter -= 2;
-      if (counter < -512) {
-        counter = -512;        
-      }
-      Joystick.setButton(0, 0);
-      Joystick.setXAxis(-counter);
-    }
-//    Serial.print("Position: ");
-//    Serial.println(counter);
+void switch3Way() {
+  int a = digitalRead(15);
+  int b = digitalRead(16);
+
+  if (a == 1 && b == 1) {
+    switchPos = 1;
+  } else if (b == 1) {
+    switchPos = 2;
+  } else {
+    switchPos = 0;
   }
-  aLastState = aState; // Updates the previous state of the outputA with the current state
 
+}
+
+void setAxis(int counter) {
+
+  counter *= sensitivity;
+  if (switchPos == 0) {
+    xPos += counter;
+    if(xPos > axisRange){
+      xPos = axisRange;
+    } else if (xPos < -axisRange){
+      xPos = -axisRange;
+    }
+    Joystick.setXAxis(xPos);
+//    Serial.print("x = ");
+//    Serial.println(xPos);
+
+  } else if (switchPos == 1) {
+    yPos += counter;
+     if(yPos > axisRange){
+      yPos = axisRange;
+    } else if (yPos < -axisRange){
+      yPos = -axisRange;
+    }
+    Joystick.setYAxis(yPos);
+//    Serial.print("y = ");
+//    Serial.println(yPos);
+  } else {
+    zPos += counter;
+     if(zPos > axisRange){
+      zPos = axisRange;
+    } else if (zPos < -axisRange){
+      zPos = -axisRange;
+    }
+    Joystick.setZAxis(zPos);
+//    Serial.print("z = ");
+//    Serial.println(zPos);
+  }
+}
+
+void rotary() {
+
+  // Read the current state of CLK
+  currentStateCLK = digitalRead(CLK);
+
+  // If last and current state of CLK are different, then pulse occurred
+  // React to only 1 state change to avoid double count
+  if (currentStateCLK != lastStateCLK  && currentStateCLK == 1) {
+
+    // If the DT state is different than the CLK state then
+    // the encoder is rotating CCW so decrement
+    if (digitalRead(DT) != currentStateCLK) {
+      if (counter > -axisRange) {
+        counter -= sensitivity;
+      }
+      setAxis(-1);
+    } else {
+      // Encoder is rotating CW so increment
+      setAxis(1);
+    }
+  }
+  // Remember last CLK state
+  lastStateCLK = currentStateCLK;
 }
